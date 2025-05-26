94c9522ea5a3779333753f776af606336a06bc74
dharmendra007 <mahantas025@gmail.com>
2025-05-26 10:53:54 +0530

    modify logout route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import {handleUserSignUp , handleUserLogin, getCurrentUser, handleLogout} from '../controllers/user.controller.js';
+import { restrictToLogedinUserOnly } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/', handleUserSignUp);
 router.post('/login', handleUserLogin);
 router.get('/me', getCurrentUser)
-router.get('/logout' , handleLogout)
+router.post('/logout' ,restrictToLogedinUserOnly, handleLogout)
 
 export default router;
