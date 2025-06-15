7c364307f3aa48fc9a2ad84d46d6976a607eb8ba
dharmendra007 <mahantas025@gmail.com>
2025-06-15 18:47:07 +0530

    update cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin : ["http://localhost:3000", "https://shorturl-frontend-lac.vercel.app" , "https://sorturl.dharmendra.space"],
+    origin : ["http://localhost:3000", "https://shorturl-frontend-lac.vercel.app" , "https://shorturl.dharmendra.space"],
     credentials : true,
     methods : ["GET", "POST", "PUT", "DELETE", "OPTIONS" , "PATCH"]
   })
